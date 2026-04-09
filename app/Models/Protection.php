01a1ca833ea0f70d34456d01a789a67194b30315
shahd <mazouzchahd12@gmail.com>
2026-04-09 19:25:02 +0100

    create protection model with relationships and fillable fields

---
+++ app/Models/Protection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Protection extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'type',
+        'rating'
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
