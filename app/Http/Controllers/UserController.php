474fe6e4b90ee070c680ff1046a1e2975a7c618d
shahd <mazouzchahd12@gmail.com>
2026-04-09 23:30:32 +0100

    add UserController with methods for user dashboard and solar panel calculation

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view ('user.dashboard');
+    }
+
+    public function calculate(Request $request) {
+        $consumption = $request->consumption;
+
+        $panelPower = 400;
+        $sunHours = 5;
+        $dailyConsumption = $consumption / 30;
+        $neededpower = $dailyConsumption / $sunHours;
+        $panelCount = ceil(($neededpower * 1000) / $panelPower);
+
+        $pricePerPanel =  1500;
+        $totalCost = $panelCount * $pricePerPanel;
+
+        return view ('user.dashboard', compact(
+            'panelCount',
+            'totalCost',
+            'consumption'
+        ));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
