ca882d30a2f6d95fe0e0ee8ac85d60b82fcb49da
shahd <mazouzchahd12@gmail.com>
2026-04-10 00:57:19 +0100

    update dashboard view to display average consumption, required panels, estimated cost, savings, and advice

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -15,8 +15,38 @@
     <button type="submit">Calculate</button>
 </form>
 
-@if(isset($panelCount))
+@if(isset($average))
     <h2>Result:</h2>
-    <p>Panels needed: {{ $panelCount }}</p>
-    <p>Estimate Cost: {{ $totalCost }} MAD</p>
+    <p>Average (3 months) {{$average}} KWh</p>
+    <p>required panels: {{$panelCount}}</p>
+    <p>Estimated cost: {{ $totalCost}} MAD</p>
+
+    <h3>Comparing</h3>
+    <p>Before solar: {{$average}} KWh</p>
+    <p>After solar: {{$afterSolar}} KWh</p>
+
+    <h3>Savings</h3>
+    <p>You save around: {{ $savings }} MAD / month</p>
+
+    <h3>Advice</h3>
+    <p>{{$advice}}</p>
 @endif
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<canvas id="consumptionChart"></canvas>
+ <script>
+    const ctx = document.getElementById('consumptionChart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Month 1', 'Month 2', 'Month 3'],
+            datasets: [{
+                label: 'consumption (KWh)',
+                data: [{{ $m1 ?? 0 }}, {{ $m2 ?? 0 }}, {{ $m3 ?? 0 }}],
+            }]
+        }
+    });
+ </script>
