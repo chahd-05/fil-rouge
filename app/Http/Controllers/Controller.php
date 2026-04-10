802023f617c3aaf3ae28718dd50cebb1de2a255d
shahd <mazouzchahd12@gmail.com>
2026-04-10 02:15:16 +0100

    add downloadPDF method to generate and download solar report as PDF

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,30 @@
 <?php
+use Barryvdh\DomPDF\Facade\Pdf;
 
-namespace App\Http\Controllers;
 
-abstract class Controller
+public function downloadPDF(Request $request)
 {
-    //
+    $m1 = (float) $request->m1;
+    $m2 = (float) $request->m2;
+    $m3 = (float) $request->m3;
+
+    $average = ($m1 + $m2 + $m3) / 3;
+    $afterSolar = $average * 0.4;
+
+    $pricePerKwh = (float) $request->price_kwh;
+
+    $currentCost = $average * $pricePerKwh;
+    $afterSolarCost = $afterSolar * $pricePerKwh;
+    $savings = $currentCost - $afterSolarCost;
+
+    $pdf = Pdf::loadView('user.report', compact(
+        'm1','m2','m3',
+        'average',
+        'afterSolar',
+        'currentCost',
+        'afterSolarCost',
+        'savings'
+    ));
+
+    return $pdf->download('solar-report.pdf');
 }
