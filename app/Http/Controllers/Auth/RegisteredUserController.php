43326a08271fa80391f4f5c5495718a4ef6dad77
chahed mazouz <mazouzchahd12@gmail.com>
2026-04-22 11:57:36 +0100

    feat: add the check role to the store function in register user controller

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,12 +33,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'role' => ['nullable', 'in:user,engineer'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'role' => $request->string('role')->value() ?: 'user',
             'password' => Hash::make($request->password),
         ]);
 
