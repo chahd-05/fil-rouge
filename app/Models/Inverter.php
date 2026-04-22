efdc4623a7cfba2ccee4a7449d243c60df324e58
chahed mazouz <mazouzchahd12@gmail.com>
2026-04-22 11:53:12 +0100

    feat: add new field to inverter model

--- app/Models/Inverter.php
+++ app/Models/Inverter.php
@@ -8,9 +8,19 @@ class Inverter extends Model
 {
     protected $fillable = [
         'project_id',
+        'model',
         'power',
+        'ac_power_kw',
+        'max_dc_power_kw',
+        'mppt_count',
+        'strings_per_mppt',
+        'mppt_min_voltage',
+        'mppt_max_voltage',
+        'max_dc_voltage',
+        'max_input_current',
+        'nominal_ac_voltage',
         'type',
-        'efficiency'
+        'efficiency',
     ];
 
     public function project()
